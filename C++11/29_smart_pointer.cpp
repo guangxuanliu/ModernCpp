b542b7055dff7bd86c5c2a4d48c6d9046c18bc3e
Liu Guangxuan <Guang-xuan.liu@leica-geosystems.com.cn>
2024-10-31 09:54:46 +0800

    Smart pointer.

---
+++ C++11/29_smart_pointer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*
+* 智能指针：
+* 博客： https://liuguangxuan.org/index.php/archives/49/
+* gist备份：https://gist.github.com/guangxuanliu/f6d7614ae1d57641c20d3ee9567af339
+*/
+
+int main()
+{
+    std::cout << "Please look at blog: " << "https://liuguangxuan.org/index.php/archives/49/" << std::endl;
+    return 0;
+}
